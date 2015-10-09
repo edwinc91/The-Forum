902abf430acf32af3b294fcb00ac5022985166a8
Edwin Choi <edwinc91@Edwin-Chois-MacBook-Pro.local>
2015-10-09 10:39:29 -0400

    added MONGOURI stuff

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var express = require('express'),
-    PORT    = process.env.PORT || 3001,
-    server  = express();
+var express   = require('express'),
+    PORT      = process.env.PORT || 3001,
+    server    = express(),
+    MONGOURI  = process.env.MONGOLAB_URI,
+    dbname    = "some_useful_name"
+    mongoose  = require('mongoose');
 
 server.get('/secret-test', function (req, res) {
   res.write("Welcome to this app");
   res.end();
 });
 
+mongoose.connect(MONGOURI + "/" + dbname);
 server.listen(PORT, function () {
   console.log("Server is running on PORT: ", PORT);
 });
