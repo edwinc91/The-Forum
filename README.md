804698fc647f025bed7401d1cc7b3c9bb5d728af
Edwin Choi <edwinc91@Edwin-Chois-MacBook-Pro.local>
2015-10-12 15:17:44 -0400

    added layout pages and also added some more lines of code in the server js file

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 TODO: Anything and Everything..
 
 Perceived Challenges: Sorting posts based on upvote/downvote
+
+I need a:
+Homepage <-- Guests/Users can see, has all of the topics
+
+Posts <--- Guests/Users can see
+
+To comment/make a new post <--- Must Log In
+
+Log In Page
+
+Register Page
--- server.js
+++ server.js
@@ -4,11 +4,22 @@ var express         = require('express'),
     MONGOURI        = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
     dbname          = "some_useful_name",
     mongoose        = require('mongoose'),
+    Schema          = mongoose.Schema,
     ejs             = require('ejs'),
-    layouts         = require('express-ejs-layouts'),
+    expressLayouts  = require('express-ejs-layouts'),
     bodyParser      = require('body-parser'),
     methodOverride  = require('method-override'),
     session         = require('express-session');
+
+var threadSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  date: Date,
+  body: { type: String, required: true },
+  comments: [{ author: String, body: String, date: Date }]
+}, {collection: 'forum_thread_list', strict: true});
+
+var Thread = mongoose.model(null, threadSchema);
 
 server.get('/secret-test', function (req, res) {
   res.write("Welcome to this app");
@@ -18,4 +29,41 @@ server.get('/secret-test', function (req, res) {
 mongoose.connect(MONGOURI + "/" + dbname);
 server.listen(PORT, function () {
   console.log("Server is running on PORT: ", PORT);
+});
+
+server.set('views', './views');
+server.set('view engine', 'ejs');
+
+server.use(express.static('./public'));
+server.use(expressLayouts);
+server.use(methodOverride('_method'));
+
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.get('/', function (req, res) {
+  Thread.find({}, function (err, allThreads) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('homepage', {
+      threads: allThreads
+      });
+    }
+  });
+});
+
+server.get('/threads/new', function (req, res) {
+  res.render('newthread');
+});
+
+server.get('/threads/:id', function (req, res) {
+  Thread.findById(req.params.id, function (err, aSpecificThread) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('showthread', {
+        thread: aSpecificThread
+      });
+    }
+  });
 });
